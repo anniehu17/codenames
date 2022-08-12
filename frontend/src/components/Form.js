59d368837a259b1c5337ef48cb02c419b6917dd1
Evan Cowin <cowin.6@buckeyemail.osu.edu>
2022-08-11 21:21:33 -0400

    Formatted project correctly so that everything is up and running

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Field = React.forwardRef(({label, type}, ref) => {
+    return (
+      <div>
+        <label className="loginLabel" >{label}</label>
+        <input ref={ref} type={type} className="loginInput" />
+      </div>
+    );
+});
+
+const Form = ({onSubmit}) => {
+    const usernameRef = React.useRef();
+    const passwordRef = React.useRef();
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        };
+        onSubmit(data);
+    };
+    return (
+      <form className="loginForm" onSubmit={handleSubmit} >
+        <Field ref={usernameRef} label="Username:" type="text" />
+        <Field ref={passwordRef} label="Password:" type="password" />
+        <div>
+          <button className="loginButton" type="submit">Submit</button>
+        </div>
+      </form>
+    );
+};
+
+export default Form
